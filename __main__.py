9a0380289db2909cc92e65d6bb253801d6b18e16
Wlodzimierz Matsydin <wlodzimierz.matsydin@volvocars.com>
2023-10-22 13:06:53 +0200

    Small refactoring
    - rework showing results
    - change structure of main function

--- __main__.py
+++ __main__.py
@@ -11,16 +11,14 @@ import logging
 import os
 import select
 import sys
+import time
 from pathlib import Path
 
 from src import utils
 from src.access_manager import Access
 
-# TODO: error when wrong password
-DISPLAY_TIMEOUT = 20
 APP_DIR = Path(f"{__file__}").parent
 CONFIG_FILE_PATH = APP_DIR / "config.conf"
-APP = "gpg"
 SEARCH_VALUE_PATTERN = r"^[A-Za-z0-9.]{3,}$"
 ADDING_VALUE_PATTERN = r"^\S{3,} \S{3,} \S{3,}$"
 
@@ -31,14 +29,17 @@ logging.basicConfig(format="%(message)s", level=logging.INFO, handlers=[logging.
 def _parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Search for credentials in an encrypted archive")
     parser.add_argument(
+        "-a",
         "--add",
-        help="Add credentials and save it in a new archive",
+        help="Update the existing credential base and save it in a new archive",
         action="store_true",
     )
     parser.add_argument(
         "-w",
-        "--work_dir",
-        help="Work directory, otherwise - default path will be used. The path will be stored in config.conf file",
+        "--work_path",
+        help="Path to either a directory or an encrypted archive to work with. "
+             "The path will be stored in config.conf file",
+        default=None,
     )
     parser.add_argument("-d", "--debug", help="Enable debug mode", action="store_true")
     return parser.parse_args()
@@ -60,7 +61,9 @@ def _search(access: Access) -> None:
         value = _get_input_value()
         if utils.is_input_valid(value=value, pattern=SEARCH_VALUE_PATTERN):
             found = access.search_in_content(value)
-            utils.pretty_print([str(item) for item in found])
+            if found:
+                utils.short_show([str(item) for item in found])
+                time.sleep(5)
 
 
 def _add(access: Access) -> None:
@@ -68,60 +71,53 @@ def _add(access: Access) -> None:
     try:
         while True:
             _log.info(f"New credentials:")
+            os.system("tput sc")
             value = _get_input_value(timeout=60)
+            os.system("tput rc && tput ed")
             if utils.is_input_valid(value=value, pattern=ADDING_VALUE_PATTERN):
                 access.add_content(value)
             else:
-                _log.info(f'Phrase "{value}" is not valid')
+                _log.info(f'Input phrase is not valid')
     except KeyboardInterrupt:
         access.encrypt_and_export_to_new_file_if_content_updated()
         raise
 
 
-def main(input_args: argparse.Namespace) -> None:
-    args_dict = vars(input_args)
-    enabled_args = [arg for arg in args_dict if args_dict[arg]]
-    if enabled_args:
-        _log.info(f"Run app with arguments: {enabled_args}")
+def _set_debug_mode() -> None:
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+    _log.info("Debug mode enabled")
 
+
+def main() -> None:
     try:
-        if input_args.debug:
-            root_logger = logging.getLogger()
-            root_logger.setLevel(logging.DEBUG)
-            _log.info("Debug mode enabled")
+        input_args = _parse_args()
 
-        if input_args.work_dir:
-            work_dir = Path(input_args.work_dir)
-            utils.add_to_config(path=CONFIG_FILE_PATH, data={"work_dir": input_args.work_dir})
-        else:
-            config_content = utils.read_config(path=CONFIG_FILE_PATH)
-            if config_content is not None and "work_dir" in config_content:
-                work_dir_value = config_content["work_dir"]
-                assert isinstance(work_dir_value, str)
-                work_dir = Path(work_dir_value)
-            else:
-                work_dir = APP_DIR
-                utils.add_to_config(path=CONFIG_FILE_PATH, data={"work_dir": str(work_dir)})
-        access = Access(work_dir)
+        if input_args.debug:
+            _set_debug_mode()
+
+        if input_args.work_path is not None:
+            if not Path(input_args.work_path).exists():
+                raise AssertionError(f"Path does not exist: {input_args.work_path}")
+            utils.add_to_config(path=CONFIG_FILE_PATH, data={"work_path": input_args.work_path}, assert_ok=True)
+        config_content = utils.read_config(path=CONFIG_FILE_PATH)
+        access = Access(Path(str(config_content["work_path"])))
 
         if input_args.add:
             _add(access)
         else:
-            if access.archive_path is not None:
+            if access.archive_path is None:
+                _log.warning("No encrypted archive found to search in")
+            else:
                 _search(access)
-    except Exception as e:
-        if _log.level == 0:
-            _log.error(f"Program failed: {e}")
-        else:
-            _log.exception("Program failed:")
+    except Exception:
+        _log.exception("Program failed:")
     except KeyboardInterrupt:
-        os.system("tput rc && tput rc && tput ed")
-        sys.exit(1)
+        sys.exit(0)
     finally:
-        # TODO: clear screen finally
+        os.system("tput setaf 7")
         _log.info("Application closed")
 
 
 if __name__ == "__main__":
-    app_args = _parse_args()
-    main(app_args)
+    main()
--- src/utils.py
+++ src/utils.py
@@ -8,9 +8,8 @@ import json
 import logging
 import os
 import re
-import time
 from pathlib import Path
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Union
 
 _log = logging.getLogger(__name__)
 
@@ -23,35 +22,28 @@ def is_input_valid(value: str, pattern: str) -> bool:
     return False
 
 
-def pretty_print(data: List[str]) -> None:
-    if not data:
-        data = ["no data"]
+def short_show(data: List[str], timeout: int = 5) -> None:
     os.system(f"tput setaf 2")
-    for item in data[:-1]:
-        _log.info(item + "\n")
-    _log.info(data[-1])
+    lines = "\n".join(data)
+    os.system(f"echo $'{lines}' | timeout --foreground {timeout} less -e")
     os.system("tput setaf 7")
-    c = 0
-    for i in data:
-        c += len(i.split("\n"))
-    c = c + 2
-    time.sleep(5)
-    os.system(f"tput cuu {c} && tput ed")
 
 
-def read_config(path: Path) -> Optional[JsonContent]:
+def read_config(path: Path) -> JsonContent:
     if not path.is_file():
         _log.warning(f"Config file does not exist or is not a file: {path}")
-        return None
+        return {}
     with open(path, encoding="utf8") as f:
         content = json.loads(f.read())
         assert isinstance(content, dict)
         return content
 
 
-def add_to_config(path: Path, data: Dict[str, Union[str, int]]) -> None:
+def add_to_config(path: Path, data: Dict[str, str], assert_ok: bool = False) -> None:
     with open(path, "w+", encoding="utf-8") as f:
         content = json.load(f) if f.read() else {}
+        if assert_ok:
+            data = {k: v for k, v in data.items() if content.get(k, None) != v}
         content.update(data)
         json.dump(content, f)
     _log.debug(f"Added data to config file: {data}")
